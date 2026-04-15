7923710c660a36e9b2d7d6d7913cdd21e307e398
Salil Thakur <salilthakur09@gmail.com>
2026-04-16 00:15:55 +0530

    changed cart section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,6 @@ import Footer from './components/Footer';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ProductListingPage from './pages/ProductListingPage';
 import ProductDetailPage from './pages/ProductDetailPage';
-import CartPage from './pages/CartPage';
 
 function App() {
   return (
@@ -16,7 +15,6 @@ function App() {
               <Route path="/" element={<HomePage />} />
               <Route path="/products/:category" element={<ProductListingPage />} />
               <Route path="/product/:id" element={<ProductDetailPage />} />
-              <Route path="/cart" element={<CartPage />} />
             </Routes>
           </div>
         </div>
