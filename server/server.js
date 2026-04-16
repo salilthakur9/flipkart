b2420ef8779dae590437fa8ff0b5098b032efbc9
Salil Thakur <salilthakur09@gmail.com>
2026-04-16 22:36:07 +0530

    added cors

--- server/server.js
+++ server/server.js
@@ -27,10 +27,7 @@ const app = express();
 // =======================================================
 app.use(express.json());
 
-app.use(cors({
-  origin: ['http://localhost:5173'],
-  credentials: true
-}));
+app.use(cors());
 
 // =======================================================
 // ✅ DEBUG ENV (OPTIONAL)
