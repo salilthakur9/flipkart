a70cd972c2eb3c90acb90e480e1af1d2b3b87040
Salil Thakur <salilthakur09@gmail.com>
2026-04-16 23:24:43 +0530

    remove razorpay

--- server/routes/orderRoutes.js
+++ server/routes/orderRoutes.js
@@ -1,14 +1,9 @@
 import express from 'express';
-import { createRazorpayOrder, verifyPayment, getUserOrders, placeOrder} from '../controllers/orderController.js';
+import { placeOrder } from '../controllers/orderController.js';
 import auth from '../middleware/auth.js';
 
 const router = express.Router();
 
-// 🔥 CREATE ORDER (RAZORPAY)
-router.post('/create-order', auth, createRazorpayOrder);
-router.get('/', auth, getUserOrders);
 router.post('/place', auth, placeOrder);
-// 🔥 VERIFY PAYMENT + SAVE ORDER
-router.post('/verify-payment', auth, verifyPayment);
 
 export default router;
